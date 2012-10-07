a4b27e2de2382b75d960de27ec48eb6ca0b718d1
Eli Finer <elig@il.ibm.com>
2012-10-07 21:39:09 +0200

    changed layout to tables, added submit message by double-enter and on timeout

--- chat.js
+++ chat.js
@@ -2,8 +2,31 @@ function submitMessage() {
     var text = document.getElementById("message").value;
     var history = document.getElementById("history");
     var item = document.createElement("p");
-    item.innerHTML = text;
+    item.innerHTML = text.replace(/\n/g, "<br>");
     history.appendChild(item);
     history.scrollTop = history.scrollHeight;
     document.getElementById("message").value = "";
+}
+
+function onMessageKey() {
+    submitTime = 0;
+    var key = window.event.keyCode;
+    var text = document.getElementById("message").value;
+    if (key === 13 && text.match(/\n$/)) {
+        submitMessage();
+        this.event.preventDefault();
+    }
+}
+
+var submitInterval = setInterval("autoSubmitMessage()", 1000);
+var submitTime = 0;
+
+function autoSubmitMessage() {
+    submitTime += 1;
+    if (submitTime > 2) {
+        submitTime = 0;
+        if (document.getElementById("message").value !== "") {
+            submitMessage();
+        }
+    }
 }
